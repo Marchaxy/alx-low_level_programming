5ad175a6a13f7b46fb962e4941f0a938a38130c1
marchaxy <marchaxy10@gmail.com>
2023-09-16 04:49:52 -0700

    basics

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,21 @@
-1   #include "main.h"
-2
-3   /**
-4    * _isupper - check if letter is uppercase
-5    * @c: print int
-6    * Return: Always 0
-7    */
-8
-9   int _isupper(int c)
-10  {
-11  if (c >= 'A' && c <= 'Z')
-12  { 
-13  return (1);
-14  }   
-15
-16  else
-17  {
-18  return (0);
-19  }
-20
-21  }
+#include "main.h"
+
+/**
+ * _isupper - check if letter is uppercase
+ * @c: print int
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+
+else
+{
+return (0);
+}
+
+}
