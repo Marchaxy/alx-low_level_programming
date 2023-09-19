d7c58033b77570aa0da424a380e936198ab1c5d2
marchaxy <marchaxy10@gmail.com>
2023-09-18 19:27:47 -0700

    fact

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,25 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- *main - prints out "Programming is like building a multilingual puzzle
- *Return: 0 if exited properly, otherwise non-zero
- **/
+ * print_rev - a function that prints a string, in reverse
+ * fcounter is to first count to end, n is to count back
+ * @s: str input
+ * Return: string in reverse
+*/
 
-int main(void)
+void print_rev(char *s)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+int fcounter = 0;
+int i, n;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+fcounter++;
+}
+
+for (n = (fcounter - 1); n >= 0; n--)
+{
+_putchar(s[n]);
+}
+_putchar('\n');
 }
